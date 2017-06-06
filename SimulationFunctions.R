20287b049d6d2b91ea5ab75012dceae2a69afe00
tpweiss06 <tpweiss06@gmail.com>
2017-06-06 14:16:08 -0500

    I added the Disperse function to implement dispersal via a Poisson process and updated the Notebook accordingly.

--- SimulationFunctions.R
+++ SimulationFunctions.R
@@ -13,14 +13,14 @@
 # ---------------------------- Biological Functions ----------------------------
 # ------------------------------------------------------------------------------
 
-######
+###### CalcDispTrait
 # This function will calculate a dispersal trait value for a set of individuals
 #    based on their dispersal alleles.
 ### INPUTS
-# population: A vector of the population (i.e. row numbers) for which to 
-#              calculate a dispersal trait.
-# PopMat: The population matrix to use
-# nDisp: The number of dispersal loci in the current simulation
+# population:  A vector of the population (i.e. row numbers) for which to 
+#                   calculate a dispersal trait.
+# PopMat:      The population matrix to use
+# nDisp:       The number of dispersal loci in the current simulation
 ### OUTPUTS
 # The function will create a matrix with two columns. The first column will 
 #    correspond to the ID (row number) of the individual and the second column
@@ -47,6 +47,73 @@ CalcDispTrait <- function(population, PopMat, nDisp){
      return(DispTrait)
 }
 
+###### Disperse
+# This function will use the previously calculated diffusion coefficients in a
+#    Poisson process to perform the actual dispersal events.
+### INPUTS
+# PopMat:      The population matrix to use
+# DispTrait:   The two column matrix created by the CalcDispTrait function
+# width:       The number of discrete cells defining the width of the word
+#                   being simulated
+# DispTime:    The total time allowed for dispersal during the Poisson Process.
+#                   This will be set to 1 by default because it really only
+#                   adjusts the scale of the diffusion coefficients so there's
+#                   no good reason to set to anything else unless we are trying
+#                   to match scales with a given system or a different model.
+### OUTPUTS
+# The function will perform dispersal for all the individuals involved via a 
+#    Poisson process in which each event is defined by an individual randomly
+#    shifting in space one cell in any direction (i.e. unbiased). This function
+#    assumes wrapping borders for the width direction to avoid edge effects.
+Disperse <- function(PopMat, DispTrait, width, DispTime = 1){
+     # First create the initial vector of event times based on each individual's
+     #    diffusion coefficient.
+     EventTimes <- rexp(n = nrow(DispTrait), rate = 1) / DispTrait[,2]
+     # Find the first event and record the individual
+     CurTime <- min(EventTimes)
+     CurIndividual <- which(EventTimes == CurTime)
+     # Record the current x and y coordinates for each individual prior to 
+     #    dispersal
+     CurX <- PopMat[DispTrait[,1], "x0"]
+     CurY <- PopMat[DispTrait[,1], "y0"]
+     # Now disperse individuals according to their event times until we exceed
+     #    the allowed dispersal time
+     StepDists <- c(-1, 1)
+     while(CurTime <= DispTime){
+          # Randomly choose a direction for dispersal
+          XorY <- rbinom(n = 1, size = 1, prob = 0.5)
+          if(XorY == 0){
+               # Dispersal occurs in the x direction
+               CurX[CurIndividual] <- CurX[CurIndividual] + sample(StepDists, size = 1)
+          }
+          if(XorY == 1){
+               # Dispersal occurs in the y direction
+               CurY[CurIndividual] <- CurY[CurIndividual] + sample(StepDists, size = 1)
+          }
+          # Add in the next event time for the current individual and reset
+          #    the current time and individual
+          EventTimes[CurIndividual] <- rexp(n = 1, rate = 1) / DispTrait[CurIndividual,2]
+          CurTime <- min(EventTimes)
+          CurIndividual <- which(EventTimes == CurTime)
+     }
+     # Use the width of the landscape to correct any x coordinates outside
+     #    of the allowed boundaries
+     while(min(CurX) < 0){
+          CurX <- ifelse(CurX < 0, CurX + width, CurX)
+     }
+     CurX <- ifelse(CurX > width, CurX %% width, CurX)
+     CurX <- ifelse(CurX == 0, width, CurX)
+     
+     # Finally, update the appropriate entries of the PopMat matrix and return
+     #    the new version.
+     PopMat[DispTrait[,1], "x1"] <- CurX
+     PopMat[DispTrait[,1], "y1"] <- CurY
+     return(PopMat)
+}
+
+
+
+
 
 # ------------------------------------------------------------------------------
 # -------------------------- Environmental Functions ---------------------------
@@ -56,15 +123,16 @@ CalcDispTrait <- function(population, PopMat, nDisp){
 # --------------------------- Bookkeeping Functions ----------------------------
 # ------------------------------------------------------------------------------
 
-######
+###### MakePopMat
 # This function generates the matrices used to track individuals in the model
 ### INPUTS
-# PopSize: a single number indicating the number of individuals the matrix needs
-#    to track
-# nFit: the number of loci defining an individual's fitness
-# nDisp: the number of loci defining an individual's dispersal ability
-# example: a boolean value indicating whether the function should simply return
-#    an example character vector containing the column names and order used
+# PopSize:     a single number indicating the number of individuals the matrix 
+#                   needs to track
+# nFit:        the number of loci defining an individual's fitness
+# nDisp:       the number of loci defining an individual's dispersal ability
+# example:     a boolean value indicating whether the function should simply 
+#                   return an example character vector containing the column 
+#                   names and order used
 ### OUTPUTS
 # This function creates a single, empty matrix wth PopSize rows and nFit + nDisp
 #    + 5 columns with appropriate names (see function definition for details). 
@@ -96,15 +164,15 @@ MakePopMat <- function(PopSize, nFit, nDisp, example = FALSE){
      return(PopMat)
 }
 
-######
+###### get_safe_ID
 # This function looks through the current working directory and finds a safe
 #    name to use that will not overwrite anything else when saving simulation
 #    results
 ### INPUTS
-# ParentDirectory: the file path to the current working directory
-# parallel: A boolean variable indicating whether the current simulations are
-#    taking place over multiple nodes so that file names should include the 
-#    name of the current node
+# ParentDirectory:  the file path to the current working directory
+# parallel:         A boolean variable indicating whether the current simulations 
+#                        are taking place over multiple nodes so that file names 
+#                        should include the name of the current node
 ### OUTPUTS
 # The full file path for a new directory that the current simulation results will
 #    be saved to
